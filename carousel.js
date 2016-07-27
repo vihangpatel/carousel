0b3fa1727bb9623f270fad8836e763f80727872c
Vihang Patel <vihang.patel@craftsvilla.com>
2016-07-27 14:01:53 +0530

    Add < and > in buttons

--- carousel.js
+++ carousel.js
@@ -83,6 +83,8 @@ autoSlide          => Autoslide option
 	Carousel.prototype.createButtons = function() {
 		this.leftButtonEle = document.createElement('div'),
 		this.rightButtonEle = this.leftButtonEle.cloneNode(true);
+		this.leftButtonEle.innerText = this.options.leftText || "<";
+		this.rightButtonEle.innerText = this.options.rightText ||">";
 		this.leftButtonEle.className = 'left-button';
 		this.rightButtonEle.className = 'right-button';
 		this.currentEl().appendChild(this.leftButtonEle);
